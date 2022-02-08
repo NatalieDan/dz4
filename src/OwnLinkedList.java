df35e9a111c6feb7a384e1752e11198638f655ba
NatalieDan <natik891@yandex.ru>
2022-02-08 20:43:15 +0300

    Initial commit

---
+++ src/OwnLinkedList.java
@@ -0,0 +1,73 @@
+public class OwnLinkedList<T> {
+    private class Node{
+        T data;
+        Node next;
+
+        public Node(T data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private long size;
+    private Node head;
+    private Node tail;
+
+    public OwnLinkedList(){
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public void addNode(T data){
+        Node newNode = new Node(data);
+        if (head == null){
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+        }
+        tail = newNode;
+        size++;
+    }
+
+    public void addNode(T data, int index){
+        Node newNode = new Node(data);
+        if (index > size){
+            addNode(data);
+            return;
+        }
+        Node current = head;
+        for (int i = 0; i < index-1; i++){
+            current = current.next;
+        }
+        Node nextNode = current.next;
+        current.next = newNode;
+        newNode.next = nextNode;
+        size++;
+    }
+
+    public void display(){
+        Node current = head;
+        if (head == null){
+            System.out.println("Односвязный список пуст");
+            return;
+        }
+        while (current != null){
+            System.out.println(current.data + " ");
+            current = current.next;
+        }
+    }
+
+    public void display(int index){
+        if (index > size){
+            System.out.println("Размер списка меньше, чем заданный индекс");
+            return;
+        }
+        Node current = head;
+        for (int i = 0; i < index; i++){
+            current = current.next;
+        }
+        System.out.println(current.data);
+    }
+}
---
+++ src/OwnLinkedListMain.java
@@ -0,0 +1,13 @@
+public class OwnLinkedListMain {
+    public static void main(String[] args) {
+        OwnLinkedList<Integer> integerOwnLinkedList = new OwnLinkedList<>();
+        integerOwnLinkedList.addNode(150);
+        integerOwnLinkedList.addNode(352);
+        integerOwnLinkedList.addNode(654);
+        integerOwnLinkedList.addNode(368);
+        integerOwnLinkedList.addNode(124);
+        integerOwnLinkedList.addNode(750);
+        integerOwnLinkedList.addNode(1111111,4);
+        integerOwnLinkedList.display();
+    }
+}
